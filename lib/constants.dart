07fc70b9bfb679a0bc92cec8d8c4a9423e268ebe
Muhammad Usama <imusamairshad@gmail.com>
2024-02-05 00:37:48 +0500

    modified.

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 const kLinear = Color(0xff92A3FD);
+const kPurpleLinear = Color(0xffC58BF2);
 const kBlack = Color(0xff1D1617);
 const kGray1 = Color(0xff7B6F72);
